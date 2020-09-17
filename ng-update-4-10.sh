056073d722c1e2d7cd74004c23a94cde281cefc6
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-17 09:49:02 -0300

    Corrige script ng-update-4-10.sh

---
+++ ng-update-4-10.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+COLOR_RED="31"
+COLOR_GREEN="32"
+COLOR_BLUE="34"
+
+print_info() {
+  echo -e "\n[\e[${COLOR_BLUE}mINFO\e[0m] $1\n"
+}
+
+print_success() {
+  echo -e "\n[\e[${COLOR_GREEN}mSUCCESS\e[0m] $1\n"
+}
+
+print_error() {
+  echo -e "\n[\e[${COLOR_RED}mERROR\e[0m] $1\n"
+}
+
+if [ -z "$1" ]; then
+  print_error "Necessário informar a aula"
+  exit
+fi
+
+AULA="$1"
+COMMIT_MESSAGE="Atualiza aula ${AULA} para o ng10"
+
+commit_ammend() {
+  git add .
+  git commit --amend -m "${COMMIT_MESSAGE}"
+}
+
+print_info "Instalando o @angular/cli@6"
+npm install @angular/cli@6 --save-dev
+
+git add .
+git commit -m "${COMMIT_MESSAGE}"
+
+print_info "Instalando o @angular/core@6 e atualizando o codelyzer"
+ng update @angular/core@6 codelyzer
+commit_ammend
+
+print_info "Instalando o rxjs 6"
+npx rxjs-tslint -p src/tsconfig.app.json
+rxjs-5-to-6-migrate -p src/tsconfig.app.json
+npm rm rxjs-compat
+commit_ammend
+
+print_info "Migrando a configuração do @angular/cli@1.7.4 para o @angular/cli@6"
+ng update @angular/cli --migrate-only --from=1.7.4
+commit_ammend
+
+print_info "Instalando o Angular 7"
+ng update @angular/cli@7 @angular/core@7
+commit_ammend
+
+print_info "Instalando o Angular 8"
+ng update @angular/cli@8 @angular/core@8
+commit_ammend
+
+print_info "Instalando o Angular 9"
+ng update @angular/core@9 @angular/cli@9
+commit_ammend
+
+print_info "Instalando o Angular 10"
+ng update @angular/core @angular/cli
+commit_ammend
+
+# ng update
+# npm outdated
+ng update bootstrap
+commit_ammend
